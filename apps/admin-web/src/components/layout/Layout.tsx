cdbcf7165e95c6e13e09bce1eb35218786a29448
wandanamaddumage <wandanamuthumali99@gmail.com>
2026-06-26 04:15:41 +0530

    feat: update admin portal responsivness

--- apps/admin-web/src/components/layout/Layout.tsx
+++ apps/admin-web/src/components/layout/Layout.tsx
@@ -40,7 +40,7 @@ export function Layout() {
 
       <div className="flex-1 flex flex-col overflow-hidden min-w-0">
         <Header onMenuClick={() => setIsMobileOpen(true)} />
-        <main className="flex-1 overflow-y-auto p-4 md:p-6">
+        <main className="flex-1 overflow-y-auto p-4 md:p-6 mx-4">
           <Outlet />
         </main>
       </div>
